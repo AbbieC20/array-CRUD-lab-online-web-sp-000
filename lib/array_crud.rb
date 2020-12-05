610024725e03c1a281da9595a51c30793e8ac884
remote-torrent-6747 <abbyc@sky.com>
2020-12-05 16:00:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   greeting = ["Hello", "Hey", "Hiya", "Hi"]
-  greeting.push("Good day")
+  greeting.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
